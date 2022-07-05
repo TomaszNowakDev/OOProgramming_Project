ad677bb6a132146f209f1e06f1303459796f0691
Tomasz Nowak <tomeknew1@gmail.com>
2022-07-05 21:29:18 +0100

    Functions: add(), searchPatient(), removePatient(), addVisit(), showPatients(), showPatientsAndVisits() added to the class Consultant.

--- model/Consultant.java
+++ model/Consultant.java
@@ -33,6 +33,52 @@ public class Consultant extends Person implements Serializable {
 		this.expertise = expertise;
 	}
 
+	public boolean add(Patient p) {
+		if (this.searchPatient(p.getName().getFullName()) == null) {
+			this.patients.add(p);
+			return true;
+		}
+		System.out.println("This patient is already on the records.");
+		return false;
+	}
+
+	public Patient searchPatient(String name) {
+		Patient obj = null;
+
+		for (Patient p : this.patients) {
+			if (p.getName().getFullName().toUpperCase().equals(name.toUpperCase())) {
+				obj = p;
+			}
+		}
+		return obj;
+	}
+	public boolean removePatient(String name) {
+		Patient remove = this.searchPatient(name);
+		if (remove != null) {
+			this.patients.remove(remove);
+			return true;
+		}
+		return false;
+	}
+
+	public void addVisit(Patient p, Visit v) {
+		p.add(v);
+	}
+
+	public void showPatients() {
+		for (int i = 0; i < patients.size(); i++) {
+			System.out.println(i + 1 + ". " + patients.get(i).getName().getLastName() + ", "
+					+ patients.get(i).getName().getFirstName());
+		}
+	}
+
+	public void showPatientsAndVisits() {
+		for (int i = 0; i < patients.size(); i++) {
+			System.out.println(i + 1 + ". " + patients.get(i).getName().getLastName() + ", "
+					+ patients.get(i).getName().getFirstName() + " Visits: " + patients.get(i).getVisits());
+		}
+	}
+
 	public String toString() {
 		String str = super.getName().getFullName() + " Expertise: " + this.expertise + ", Patients:" + this.patients;
 		return str;
