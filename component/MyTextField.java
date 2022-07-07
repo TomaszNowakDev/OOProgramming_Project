94008d293c430a06b3622ede0138b96d6a091587
Tomasz Nowak <tomeknew1@gmail.com>
2022-07-07 22:09:24 +0100

    Tooltip and validatian to MyTexField added.

--- component/MyTextField.java
+++ component/MyTextField.java
@@ -1,6 +1,7 @@
 package component;
 
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 
 public class MyTextField extends TextField {
 
@@ -9,4 +10,27 @@ public class MyTextField extends TextField {
 		// style application for class from css file
 		this.getStyleClass().add("text-field");
 	}
+
+	// validating input fields
+	public boolean validate() {
+		// adding error if text is not valid
+		if (this.getText().equals("")) {
+			this.getStyleClass().add("error");
+			this.addToolTip("Please fill out this field");
+			return false;
+		} else {
+			// removing error style if field become valid
+			this.getStyleClass().remove("error");
+			this.setTooltip(null);
+			return true;
+		}
+	}
+
+	// adding ToolTip
+	public void addToolTip(String message) {
+		final Tooltip tooltip = new Tooltip();
+		tooltip.setText(message);
+		this.setTooltip(tooltip);
+	}
+
 }
