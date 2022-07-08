97aff6915e6f60c258f8b50a6f79a1893856ba96
Tomasz Nowak <tomeknew1@gmail.com>
2022-07-08 11:35:57 +0100

    Function addListView() implemented.

--- view/MyHorizontalBox.java
+++ view/MyHorizontalBox.java
@@ -2,12 +2,17 @@ package view;
 
 import java.io.Serializable;
 import component.MyButton;
+import controller.AppController;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -31,7 +36,25 @@ public class MyHorizontalBox extends HBox implements Serializable {
 
     private void initUI() {
         // adding to the hbox
-        this.getChildren().addAll(addVBox());
+        this.getChildren().addAll(addListView(), addVBox());
+    }
+
+    private ListView<String> addListView() {
+        this.listView = new ListView<String>();
+        this.getStyleClass().add("listView");
+        this.listView.setPadding(new Insets(20, 40, 20, 20));
+        this.listView.prefHeight(300);
+        this.listView.prefWidth(100);
+        this.listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                if (AppController.getInstance().getConsultantData().size() > 0)
+                    selectedIndex = listView.getSelectionModel().getSelectedIndex();
+            }
+        });
+        this.setupListViewData();
+        return listView;
     }
 
     private VBox addVBox() {
