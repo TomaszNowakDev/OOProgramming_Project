97a29839106bbd9ca442dc280b86ce48de770a3c
Tomasz Nowak <tomeknew1@gmail.com>
2022-07-09 12:35:24 +0100

    All the fields and the constructor in MyHorizontalBox2 implemented.

--- view/MyHorizontalBoxTab2.java
+++ view/MyHorizontalBoxTab2.java
@@ -3,5 +3,15 @@ package view;
 import java.io.Serializable;
 
 public class MyHorizontalBoxTab2 extends HBox implements Serializable {
+    private VBox vb;
+    private ListView<String> listView;
+    private MyButton addConsultant, editConsultant, deleteConsultant;
+    private int selectedIndex = -1;
+    protected ListProperty<String> listProperty = new SimpleListProperty<>();
 
+    public MyHorizontalBoxTab2() {
+        super();
+        this.deleteConsultant = new MyButton("Delete Consultant");
+        this.editConsultant = new MyButton("Edit Consultant");
+    }
 }
