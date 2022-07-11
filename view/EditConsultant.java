d2b921216a5166f34b5cd9bce7876e4cf0862170
Tomasz Nowak <tomeknew1@gmail.com>
2022-07-11 20:47:06 +0100

    All the fields and the constructor implemented for class EditConsultant.

--- view/EditConsultant.java
+++ view/EditConsultant.java
@@ -1,7 +1,25 @@
 package view;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import model.Consultant;
 
 public class EditConsultant extends VBox {
+    private MyLabel label1, label2, label3, label4, label5;
+    private MyTextField text1, text2, text3, text4, text5;
+    private GridPane grid;
+    private HBox hb;
+    protected Consultant selectedConsultant;
+    private MyButton saveButton, cancelButton;
+
+    public EditConsultant(Consultant c) {
+        this.selectedConsultant = c;
+        this.text1 = new MyTextField();
+        this.text2 = new MyTextField();
+        this.text3 = new MyTextField();
+        this.text4 = new MyTextField();
+        this.text5 = new MyTextField();
+    }
 
 }
