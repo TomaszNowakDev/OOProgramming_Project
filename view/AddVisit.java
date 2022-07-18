bef3f2a67afc6b47297da640e4aba48f74e50b36
Tomasz Nowak <tomeknew1@gmail.com>
2022-07-18 16:37:26 +0100

    Function initUI() implemented.

--- view/AddVisit.java
+++ view/AddVisit.java
@@ -1,8 +1,13 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 import model.Patient;
 import model.Visit;
@@ -21,5 +26,23 @@ public class AddVisit extends VBox {
 		super();
 		this.patient = p;
 		this.submit = new MyButton("Submit");
-		}
+		initUI();
+	}
+
+	private void initUI() {
+
+		this.setPadding(new Insets(50, 20, 20, 20));
+		this.setSpacing(20);
+
+		this.text = new Text("Recording Visit for Patient: " + patient.getName().getFullName());
+		this.text.setFont(Font.font("Lato", FontPosture.ITALIC, 17));
+		this.text2 = new Text("Date of Visit: " + LocalDate.now());
+		this.text2.setFont(Font.font("Lato", FontPosture.ITALIC, 17));
+
+		this.textArea = new TextArea();
+		this.textArea.setPromptText("Add notes to the Visit");
+		this.textArea.setWrapText(true);
+
+		this.getChildren().addAll(text2, text, textArea, addHbox());
+	}
 }
